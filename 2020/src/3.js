c2aabae764fb5283fc77e5b7ff46082467ea1cad
Matt Williams <matt@williams-tech.net>
2020-12-03 02:15:45 -0500

    day 3

--- 2020/src/3.js
+++ 2020/src/3.js
@@ -1,8 +1,35 @@
 const { readInput } = require('./utils')
 
-const partOne = (rawInput) => {
-    const data = rawInput.split('\n')
-    if (data) { return true }
+const buildGrid = (rawInput, slope) => {
+    const startGrid = rawInput.split('\n').map(x=>x.split(''))
+    let endGrid = startGrid
+
+    while (endGrid.length / endGrid[0].length > slope) {
+        for (let row = 0; row < endGrid.length; row++) {
+            endGrid[row] = [ ...endGrid[row], ...startGrid[row] ]
+        }
+    }
+    
+    return endGrid 
+}
+
+const partOne = (rawInput, xMove, yMove) => {
+    const slope = yMove / xMove
+    const grid = buildGrid(rawInput, slope)
+
+    let y = 0
+    let x = 0
+    let trees = 0
+
+    while (y < grid.length && x < grid[0].length) {
+        if (grid[y][x] === '#') {
+            trees += 1
+        }
+
+        y += yMove
+        x += xMove
+    }
+    return trees 
 }
 
 const partTwo = (rawInput) => {
@@ -17,6 +44,14 @@ module.exports = {
 
 if (require.main === module) {
     const input = readInput('../input/3.txt')
-    console.log('partOne:', partOne(input))
-    console.log('partTwo:', partTwo(input))
+    console.log('partOne:', partOne(input, 3, 1))
+
+    const a = partOne(input, 1, 1)
+    const b = partOne(input, 3, 1) 
+    const c = partOne(input, 5, 1) 
+    const d = partOne(input, 7, 1) 
+    const e = partOne(input, 1, 2) 
+
+    const answer = a * b * c * d * e
+    console.log('partTwo:', answer)
 }
--- 2020/tests/3.test.js
+++ 2020/tests/3.test.js
@@ -4,9 +4,17 @@ const { partOne, partTwo } = require('../src/3')
 const input = readInput('../tests/fixtures/3.txt')
 
 test('Day 3, part 1 returns correct answer with sample data', () => {
-    expect(partOne(input)).toBe(true)
+    expect(partOne(input, 3, 1)).toBe(7)
 })
 
 test('Day 3, part 2 returns correct answer with sample data', () => {
-    expect(partTwo(input)).toBe(true)
+    const a = partOne(input, 1, 1) 
+    const b = partOne(input, 3, 1) 
+    const c = partOne(input, 5, 1) 
+    const d = partOne(input, 7, 1) 
+    const e = partOne(input, 1, 2) 
+
+    const answer = a * b * c * d * e
+
+    expect(answer).toBe(336)
 })
